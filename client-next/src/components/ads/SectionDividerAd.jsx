da054f47146fbb5e1b4e1bec613a4cdc06dee616
SumeetB <150938727+ChintutheCodeMaster@users.noreply.github.com>
2026-09-18 01:59:13 +0530

    change of plans

--- client-next/src/components/ads/SectionDividerAd.jsx
+++ client-next/src/components/ads/SectionDividerAd.jsx
@@ -4,10 +4,12 @@ const HOUSE_AD_HREF = 'https://coverageprofessor.com/';
 
 export default function SectionDividerAd() {
   return (
-    <div className="w-full my-12 h-[150px] lg:h-[200px]">
-      <a href={HOUSE_AD_HREF} target="_blank" rel="noreferrer" className="block w-full h-full">
-        <Image src="/ad.png" alt="Advertisement" width={1200} height={200} className="w-full h-full object-cover border border-[var(--line)] rounded-sm" />
-      </a>
+    <div className="w-full my-12 flex justify-center">
+      <div className="w-full max-w-[900px] aspect-[1742/463]">
+        <a href={HOUSE_AD_HREF} target="_blank" rel="noreferrer" className="block w-full h-full">
+          <Image src="/ad.png" alt="Advertisement" width={1742} height={463} className="w-full h-full object-contain border border-[var(--line)] rounded-sm" />
+        </a>
+      </div>
     </div>
   );
 }
--- client-next/src/components/ads/TopAdBanner.jsx
+++ client-next/src/components/ads/TopAdBanner.jsx
@@ -5,9 +5,9 @@ const HOUSE_AD_HREF = 'https://coverageprofessor.com/';
 export default function TopAdBanner() {
   return (
     <div className="w-full bg-[var(--bg-2)] flex justify-center py-4 border-b border-[var(--line)]">
-      <div className="w-full max-w-[970px] h-[150px]">
+      <div className="w-full max-w-[728px] aspect-[1742/463]">
         <a href={HOUSE_AD_HREF} target="_blank" rel="noreferrer" className="block w-full h-full">
-          <Image src="/ad.png" alt="Advertisement" width={970} height={150} className="w-full h-full object-cover" priority />
+          <Image src="/ad.png" alt="Advertisement" width={1742} height={463} className="w-full h-full object-contain" priority />
         </a>
       </div>
     </div>
